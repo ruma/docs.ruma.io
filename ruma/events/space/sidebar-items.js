0d7c1afe53795d6f495f4b955e4f177b9e56c33f
jplatte <jplatte@users.noreply.github.com>
2022-02-06 12:18:03 +0000

    Deploying to main from @ ruma/ruma@2de652049d094a86df5c644b5193e988ffb5d0d3 🚀

--- ruma/events/space/sidebar-items.js
+++ ruma/events/space/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["child","Types for the `m.space.child` event."],["parent","Types for the `m.space.child` event."]]});
+initSidebarItems({"mod":[["child","Types for the `m.space.child` event."],["parent","Types for the `m.space.parent` event."]]});
