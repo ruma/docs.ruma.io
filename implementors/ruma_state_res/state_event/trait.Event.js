5fb62e64a18e00bc30360a81d181c440502c35cb
jplatte <jplatte+git@posteo.de>
2022-05-09 10:46:19 +0000

    Deploying to main from @ ruma/ruma@abf735093cef5a3a7e83e8bab01334f6a5eb0ef2 🚀

--- implementors/ruma_state_res/state_event/trait.Event.js
+++ implementors/ruma_state_res/state_event/trait.Event.js
@@ -1,3 +1,4 @@
 (function() {var implementors = {};
 implementors["ruma"] = [];
+implementors["ruma_state_res"] = [];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
